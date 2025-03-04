fe89ad672dee60f24f0c998e304473011491f6bb
unknown <amaljoy@cygnonex.com>
2025-03-04 15:49:07 +0530

    new

--- controller/supportAgentController.js
+++ controller/supportAgentController.js
@@ -174,7 +174,7 @@ exports.addSupportAgent = async (req, res, next) => {
       return res.status(400).json({ message: ` ${duplicateCheck}` });
     }
 
-    const supervisor = await Supervisor.findOne({ region: data.region, status: "Deactive" });
+    const supervisor = await Supervisor.findOne({ region: data.region, status: "Active" });
 
     // Check which manager is missing and send a specific error response
     if (!supervisor) {
