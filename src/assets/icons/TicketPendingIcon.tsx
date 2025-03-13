90dcf1858de8f6589cddd81c893dda5415100d7a
SanalCygnoz <sanalk2003@gmail.com>
2025-03-13 18:28:12 +0530

    Add new ticket status icons: Pending, In Progress, Open, Solved

---
+++ src/assets/icons/TicketPendingIcon.tsx
@@ -0,0 +1,17 @@
+
+type Props = {}
+
+function TicketPendingIcon({ }: Props) {
+    return (
+        <div>
+            <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="34" height="34" rx="8" fill="#B6A2E5" />
+                <rect x="1.5" y="1.5" width="31" height="31" rx="6.5" stroke="#CAD1F1" stroke-opacity="0.8" stroke-width="3" />
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M11.75 10.25L11.75 22.25H23.75V23.75H10.25V10.25H11.75ZM20.4734 13.4741L23.6962 16.4489L22.6788 17.5511L21.026 16.0257L19.528 19.5848L17.1987 17.45L15.1952 21.125H12.5V19.625H14.3045L16.8004 15.0496L18.9717 17.0398L20.4734 13.4741ZM14.75 11.75V13.25H13.25V11.75H14.75ZM17.75 11.75V13.25H16.25V11.75H17.75ZM20.75 11.75V13.25H19.25V11.75H20.75ZM23.75 11.75V13.25H22.25V11.75H23.75Z" fill="#FEFDFA" />
+            </svg>
+
+        </div>
+    )
+}
+
+export default TicketPendingIcon
