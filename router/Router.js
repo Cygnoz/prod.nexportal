43174f9b6f9a2c929236fee6087bf67cbfd1a91f
unknown <amaljoy@cygnonex.com>
2025-03-17 09:16:17 +0530

    calling

--- router/Router.js
+++ router/Router.js
@@ -68,4 +68,8 @@ router.get('/targetAchieved',verifyToken,checkPermission('View Target'), targetC
 
 router.get('/yearlyTargets',verifyToken,checkPermission('View Target'), targetController.getYearlyTargets);
 
+
+router.post('/initiate-call',verifyToken,checkPermission('Initiate Call'),TicketController.initiateCall,ActivityLogGeneration('Initiate Call'))
+
+
 module.exports = router
