c46b8756d088166c24a89b6a66554c4065bbd11e
unknown <amaljoy@cygnonex.com>
2025-03-21 09:58:40 +0530

    new

--- server.js
+++ server.js
@@ -47,3 +47,11 @@ server.listen(PORT,()=>{
 
 })
 
+// General error handling middleware
+server.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).json({
+        success: false,
+        message: err.message || "Internal Server Error",
+    });
+});
